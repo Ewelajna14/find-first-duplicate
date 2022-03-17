5d58b4f388b42a34923b0b032028511da5f2c9b0
Ewelina <ekowalczyk1994@gmail.com>
2022-03-16 20:32:35 -0500

    done

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,11 +1,21 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+
+    const duplicated = arr.find((item, index) => 
+     arr.indexOf(item) !== index)
+   if (duplicated){
+   return duplicated
+   }
+   else{
+     return -1
+   }
+   
+ 
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+  console.log("Expecting: 4");
+  console.log("=>", findFirstDuplicate([2, 1, 4, 4, 2]));
 
   console.log("");
 
@@ -16,4 +26,15 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+// if there is duplicate
+//Input: [2, 1, 3, 3, 2]
+//Output: 3
+// [a,b,c,d,e,f]
+// if a = b return a
+// if a =c return a ...
+// check the index of each element 
+
+//Input: [1, 2, 3, 4]
+//Output: -1
+
 // And a written explanation of your solution
